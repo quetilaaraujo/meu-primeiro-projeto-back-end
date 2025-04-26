9f985b70759ad0deb0b2d0e36c18891c5886f752
quetilaaraujo <quetila.ara@gmail.com>
2025-04-26 20:09:07 -0300

    insere a nova versão do projeto integrado com o mongodb

---
+++ mulherModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const MulherSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    imagem: {
+        type: String,
+        required: true
+    },
+    minibio: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('diva', MulherSchema)
